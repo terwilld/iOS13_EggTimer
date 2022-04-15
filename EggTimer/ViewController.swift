9a420fa5973fce68e50228eb44ffebdb00efe1c2
David Terwilliger <terwilld@gmail.com>
2022-04-15 12:18:45 -0400

    First couple of sections

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,76 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+        
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    let eggTimes = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
+    var secondsRemaining = 30
     
 
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("HELLO")
+
+
+        
+        
+        
+    }
+
+
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+//        print("David is dumb")
+//        print(sender.currentTitle!)
+//        print("before dic")
+//        print(eggTimes[hardness]!)
+//        print("after dic")
+        
+        
+        switch hardness{
+            
+        case "Soft":    print(eggTimes["Soft"]!)
+            secondsRemaining = eggTimes["Soft"]!
+            
+        case "Medium":  print(eggTimes["Medium"]!)
+            secondsRemaining = eggTimes["Medium"]!
+            
+        case "Hard":    print(eggTimes["Hard"]!)
+            secondsRemaining = eggTimes["Hard"]!
+            
+        default: print ("Error")
+        }
+        
+        if (hardness == "Soft"){
+            print(eggTimes["Soft"]!)
+        }
+        
+        if (hardness == "Medium"){
+            print(eggTimes["Medium"]!)
+        }
+        
+        if (hardness == "Hard"){
+            print(eggTimes["Hard"]!)
+        }
+        
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+             if self.secondsRemaining > 0 {
+                 print ("\(self.secondsRemaining) seconds")
+                 self.secondsRemaining -= 1
+             } else {
+                 Timer.invalidate()
+             }
+         }
+        
+    }
+    
 }
